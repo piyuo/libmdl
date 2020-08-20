88de222865a05c4b5f59fabf3f80fd3aa7a2ef61
piyuo <piyuo.com@gmail.com>
2020-08-19 23:32:37 -0700

    add status error code

--- mdl/status.go
+++ mdl/status.go
@@ -4,26 +4,42 @@ package mdl
 //
 type Status uint8
 
+// ErrorAccountSuspend is account suspend error code to client
+//
+const ErrorAccountSuspend = "ACCOUNT_SUSPEND"
+
+// ErrorAccountCanceled is account canceled error code to client
+//
+const ErrorAccountCanceled = "ACCOUNT_CANCELED"
+
+// ErrorUserLeave is account suspend error code to client
+//
+const ErrorUserLeave = "USER_LEAVE"
+
+// ErrorUserCanceled is user canceled error code to client
+//
+const ErrorUserCanceled = "USER_CANCELED"
+
 const (
 	// StatusNone is no status
 	//
 	StatusNone Status = 0
 
-	// StatusAccountSuspend mean account not renew in time and wait for recycle
-	//
-	StatusAccountSuspend Status = 1
-
 	// StatusActive mean account/user is active and work normally
 	//
-	StatusActive Status = 2
+	StatusActive Status = 1
 
-	// StatusUserResign mean user has been leave this account permanently
+	// StatusAccountSuspend mean account not renew in time and wait for recycle
 	//
-	StatusUserResign Status = 3
+	StatusAccountSuspend Status = 2
 
 	// StatusAccountCanceled mean accont has problem and has been canceled manually. this account will not recycle and close permanently
 	//
-	StatusAccountCanceled Status = 4
+	StatusAccountCanceled Status = 3
+
+	// StatusUserLeave mean user has been leave this account permanently
+	//
+	StatusUserLeave Status = 4
 
 	// StatusUserCanceled mean user has problem and has been canceled manually. this user will not recycle and close permanently
 	//
