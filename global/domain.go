7e1e49c0efe4198ae133aed98888eb5f2c620da8
piyuo <piyuo.com@gmail.com>
2021-05-06 13:50:25 -0700

    move domain function to auth

--- global/domain.go
+++ global/domain.go
@@ -1,12 +1,7 @@
 package global
 
 import (
-	"context"
-	"strings"
-
 	"github.com/piyuo/libsrv/db"
-	"github.com/piyuo/libsrv/log"
-	"github.com/pkg/errors"
 )
 
 // Domain keep all registered domain name
@@ -25,65 +20,4 @@ func (c *Domain) Factory() db.Object {
 //
 func (c *Domain) Collection() string {
 	return "Domain"
-}
-
-// CreateDomain create domain name, must run in transaction
-//
-//	err := CreateDomain(ctx,tx,"a@b.c")
-//
-func CreateDomain(ctx context.Context, tx db.Transaction, domainName, accountID string) error {
-	d := &Domain{}
-	d.SetAccountID(accountID)
-	d.SetID(strings.ToLower(domainName))
-	if err := tx.Set(ctx, d); err != nil {
-		return errors.Wrap(err, "new domain")
-	}
-	return nil
-}
-
-// IsDomainTaken return true if domain already taken
-//
-//	taken, err := IsDomainTaken(ctx, "a@b.c")
-//
-func IsDomainTaken(ctx context.Context, domainName string) (bool, error) {
-	client, err := Client(ctx)
-	if err != nil {
-		return false, err
-	}
-	return client.Exists(ctx, &Domain{}, strings.ToLower(domainName))
-}
-
-// DeleteDomain remove domain name
-//
-//	err := DeleteDomain(ctx,"a@b.c")
-//
-func DeleteDomain(ctx context.Context, domainName string) error {
-	client, err := Client(ctx)
-	if err != nil {
-		return err
-	}
-	d := &Domain{}
-	d.SetID(strings.ToLower(domainName))
-	if err := client.Delete(ctx, d); err != nil {
-		return errors.Wrap(err, "delete domain")
-	}
-	return nil
-}
-
-// DeleteDomainByAccountID remove domain name by accountID
-//
-//	err := DeleteDomainByAccountID(ctx,"accountID")
-//
-func DeleteDomainByAccountID(ctx context.Context, accountID string) error {
-	client, err := Client(ctx)
-	if err != nil {
-		return err
-	}
-	done, numDeleted, err := client.Query(&Domain{}).Where("AccountID", "==", accountID).Delete(ctx, 100)
-	if done {
-		log.Info(ctx, "del %v domain ", numDeleted)
-		return err
-	}
-	log.Warn(ctx, "del domain not done, only del %v", numDeleted)
-	return err
 }
